fafe0272944f1e2eb75161befcc5cc4b4be8d129
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-11-15 09:23:02 +0900

    practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+/* useDispatch Hook을 react-redux에서 들고 온다.
+사용할 영역 안에서 dispatch 객체로 선언한다. */
+import { useDispatch } from "react-redux";
+
 function App() {
+  const dispatch = useDispatch();
+
+  /* dispatch 안에 인수를 둔다.
+  type 인수는 반드시 있어야 한다.
+
+  payload는 불러올 값을 뜻하는 것으로, 의무는 아니다. */
+  const increase = () => {
+    dispatch({type: "", payload: ""})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,11 @@
+let initialState = {
+    count: 0
+}
+
+/* state를 initialState 객체로 초기화 해주기 */
+function reducer(state = initialState, action) {
+
+}
+
+/* reducer를 내보내주기 */
+export default reducer;
